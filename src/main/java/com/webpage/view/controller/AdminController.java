567f65e2a4205553cf1668d1e5ea5b041b4b1b1a
ruine <ruineriaelysion@gmail.com>
2022-02-10 21:19:29 +0900

    update common view

---
+++ src/main/java/com/webpage/view/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.webpage.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/Index")
+    public String admin() {
+        return "admin/Index";
+    }
+
+}
