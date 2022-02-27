2e843845d5ff22fb6389234ab62e88e50d7f43ec
ruine <ruineriaelysion@gmail.com>
2022-02-27 18:18:05 +0900

    초기데이터 insert query 파일 생성

---
+++ src/main/resources/database_init_query.sql
@@ -0,0 +1,4 @@
+INSERT INTO companyInfo (companyName, companyLeader, companyAddress, companyRegNum, companyMainPhone, companyEmail, companyFax) VALUES ('', '', '', '', '', '', '');
+
+INSERT INTO menuList (menuName) VALUES ('');
+INSERT INTO tabList (menuId, tabName) VALUES ('', '');
